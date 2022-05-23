c9073de2cc9eccd5a8a89f838ff562eaa695073c
Alexandr <nasanovicha@gmail.com>
2022-05-24 00:14:12 +0300

    feat: add 04 task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,35 @@
+//task 04
+const fs = require('fs');
+const path = require('path');
 
+copyDir()
+function copyDir() {
+    fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true }, () => {
+        fs.readdir(path.join(__dirname, 'files'), {withFileTypes: true}, (err, dirList) => {
+            if(!err) {
+                
+                dirList.forEach(item => {
+                    
+                    fs.readFile(path.resolve(__dirname, 'files', item.name), {encoding: 'utf-8'}, (err, data) => {
+                        if(!err) {
+                            fs.writeFile((path.resolve(__dirname, 'files-copy', item.name)), data, err => {
+                                if (err) throw err
+                            })
+                        }
+                        else throw err
+                    }
+                    )  
+                })
+               
+            } else throw err
+        })
+    });
+
+    // fs.readdir(path.join(__dirname, 'files-copy'), {withFileTypes: true}, (err, dirList) => {
+    //     let arrCopied = [];
+    //     if(!err){
+    //         dirList.forEach(item => arrCopied.push(item.name))
+    //     }
+    //     else throw err
+    // })
+}
